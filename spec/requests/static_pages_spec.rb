3b8580556f48aac3b31edb4643673891403dccd0
Jeremy Greer <jeremygreer@reergymerej.com>
2013-12-29 21:05:40 -0500

    add Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -58,4 +58,17 @@ describe "Static pages" do
 			expect(page).to have_title("| About")
 		end
 	end
+
+	describe "Contact page" do
+
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact');
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title('| Contact')
+		end
+	end
 end
