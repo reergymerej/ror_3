00784d263e1b197d3ad77e2d81feb4b887dba19b
Jeremy Greer <jeremygreer@reergymerej.com>
2013-12-31 12:47:57 -0500

    chapter 5 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,1 @@
-def full_title(page_title)
-	base_title = "This is a test app."
-
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
+include ApplicationHelper
